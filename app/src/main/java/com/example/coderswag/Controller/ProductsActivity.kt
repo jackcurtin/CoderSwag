d69af031901219e4a8ec1c6425fed7b3a6e0f371
Jack <jackcurtin94@gmail.com>
2021-11-19 08:33:32 -0600

    created product adapater, made product grid

--- app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,45 @@
 package com.example.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coderswag.Adapters.ProductAdapter
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
 import com.example.coderswag.Utilities.EXTRA_CATEGORY
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductAdapter
+    lateinit var productsListView : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
+        productsListView = findViewById(R.id.productsListView)
+
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductAdapter(this, DataService.getProducts(categoryType!!))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+
+
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
